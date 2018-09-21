a79db564f3e0f2e57163f54a67b7d7856a7075d2
devanand73 <devanand73@yahoo.co.uk>
2018-09-21 21:20:29 +0530

    doc string,tuples and sys_arg usage example in python

--- sys_arg.py
+++ sys_arg.py
@@ -3,7 +3,21 @@ import sys
 print ('The command line arguments are:')
 for i in sys.argv:
 	print (i)
+for p in sys.path :
 
-print ('\n\nThe PYTHONPATH is','\n', sys.path)
+	print('there are:=' + str(len(sys.path)) + ' pythonpath entries')
+	print (p)
 
-os.exists 
+	print('\n')
+
+
+
+
+
+
+
+
+
+
+
+
